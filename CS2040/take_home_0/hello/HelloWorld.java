0dfba7d34d484e3ff1fa5c8ca6e06609952df393
Larry Law <larrylaw97@gmail.com>
2019-08-11 20:39:35 +0800

    chore: hello

--- CS2040/take_home_0/hello/HelloWorld.java
+++ CS2040/take_home_0/hello/HelloWorld.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * author	: 
@@ -9,6 +10,50 @@ import java.util.*;
 
 public class HelloWorld {
 
+	public static void handleQuery(String logic, int i, int j) {
+		int result = 0;
+		switch (logic) {
+		case "AND":
+			result = i == 1 && j == 1 ? 1 : 0;
+			break;
+		case "OR":
+			result = i == 1 || j == 1 ? 1 : 0;
+			break;
+		}
+		System.out.println(result);
+	}
+
+	// Logic to handle what is scanned
 	public static void main(String[] args) {
+		// switch cases for three different methods
+		Scanner sc = new Scanner(System.in);
+		int method = sc.nextInt();
+		switch (method) {
+		case 1:
+			int N = sc.nextInt();
+			for (int x = 0; x < N; x++) {
+				String logic = sc.next();
+				int i = sc.nextInt();
+				int j = sc.nextInt();
+				HelloWorld.handleQuery(logic, i, j);
+			}
+			break;
+		case 2:
+			while (sc.hasNext("(OR)|(AND)")) {
+				String logic = sc.next();
+				int i = sc.nextInt();
+				int j = sc.nextInt();
+				HelloWorld.handleQuery(logic, i, j);
+			}
+			break;
+		case 3:
+			while (sc.hasNext()) {
+				String logic = sc.next();
+				int i = sc.nextInt();
+				int j = sc.nextInt();
+				HelloWorld.handleQuery(logic, i, j);
+			}
+		}
+		sc.close();
 	}
 }
