504bcbf214c1d2e131cf94a9d672c95dfc7441f4
Larry Law <larrylaw97@gmail.com>
2019-08-14 11:35:13 +0800

    chore: subsets

--- CS2040/take_home_2/generate/Generate.java
+++ CS2040/take_home_2/generate/Generate.java
@@ -8,7 +8,19 @@ import java.util.*;
 
 public class Generate {
 	private ArrayList<String> permutations;
+	private ArrayList<String> subsets;
 	private String original;
+
+	private void generateSubsets(String result, int index) {
+		// Base Case: Iterated through string
+		if (index == original.length()) {
+			subsets.add(result);
+		} else {
+			// take or don't take
+			generateSubsets(result + original.charAt(index), index + 1);
+			generateSubsets(result, index + 1);
+		}
+	}
 
 	private void generatePermutations(String current, boolean[] isUsed) {
 		if (current.length() == original.length()) {
@@ -27,10 +39,17 @@ public class Generate {
 		Scanner sc = new Scanner(System.in);
 		original = sc.next();
 		permutations = new ArrayList<>();
+		subsets = new ArrayList<>();
 		boolean[] isUsed = new boolean[original.length()];
 		generatePermutations("", isUsed);
 		Collections.sort(permutations);
 		permutations.forEach(x -> System.out.println(x));
+
+		generateSubsets("", 0);
+		Collections.sort(subsets);
+		subsets.forEach(x -> System.out.println(x));
+
+		sc.close();
 	}
 
 	public static void main(String[] args) {
